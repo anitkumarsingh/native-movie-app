2bae3803843ff690e70a47ceeeacc7bf597f12d9
Anit <anit@Priyeshs-MacBook-Pro.local>
2026-01-16 12:37:12 +0530

    custom navigation

---
+++ app/movie/[id].tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+
+// create a component
+const MovieDetail = () => {
+  return (
+    <View style={styles.container}>
+      <Text>MovieDetail</Text>
+    </View>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2c3e50",
+  },
+});
+
+//make this component available to the app
+export default MovieDetail;
